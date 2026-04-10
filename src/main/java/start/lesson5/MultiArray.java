11fda4e71bdff6e29df07baf0401ed75fdd6090b
Almas <professor_z@mail.ru>
2026-04-10 21:17:34 +0500

    lesson5 While, MultiArray

---
+++ src/main/java/start/lesson5/MultiArray.java
@@ -0,0 +1,24 @@
+package start.lesson5;
+
+public class MultiArray {
+    public static void main(String[] args) {
+        //Ограбили магазин. Вор был ростом 178.
+        //Следователь нашел две группы людей около магазина. Задача найти вора по росту.
+        int[] group1 = {178, 179, 190};
+        int[] group2 = {165, 178, 193};
+
+
+        int[][] multiArray = {group1, group2};
+        upperFor:
+        for (int[] array : multiArray) {
+            for (int number : array) {
+                if (number == 178){
+                    System.out.println("Нашли вора!");
+                    break upperFor;
+                } else {
+                    System.out.println("Человек не подошел по росту");
+                }
+            }
+        }
+    }
+}
