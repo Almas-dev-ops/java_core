96d756da842c96b1a02ab039eae3987706c3edf6
Almas <professor_z@mail.ru>
2026-04-10 19:37:07 +0500

    lesson2 var

---
+++ src/main/java/start/lesson2/Var.java
@@ -0,0 +1,16 @@
+package start.lesson2;
+
+public class Var {
+    public static void main(String[] args) {
+        int countApples;
+        countApples = 3;
+        System.out.println(countApples);
+
+
+        var countPeople = 15;
+        System.out.println(countPeople);
+
+        var letter = 'B';
+        System.out.println(letter);
+    }
+}
