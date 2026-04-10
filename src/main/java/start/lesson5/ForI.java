3519fcaf3d90464317028a2a90edd892fab48a30
Almas <professor_z@mail.ru>
2026-04-10 20:58:17 +0500

    lesson5 ForI

---
+++ src/main/java/start/lesson5/ForI.java
@@ -0,0 +1,11 @@
+package start.lesson5;
+
+public class ForI {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7,8};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("№ " + numbers[i] + ", ");
+        }
+    }
+}
