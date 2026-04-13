b27aa85f73fc1859e78050b75f80ee9d24be3e3b
Almas <professor_z@mail.ru>
2026-04-13 21:50:49 +0500

    equals

---
+++ src/main/java/oop/class_object/equals/Child.java
@@ -0,0 +1,34 @@
+package oop.class_object.equals;
+
+public class Child {
+    private String name;
+    private int year;
+
+    public Child(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean equals(Object obj) {
+        System.out.println("Началось сравнение с " + this.getName());
+        if (obj == null){
+            return false;
+        }
+        if (obj instanceof Child child2){
+            return this.getName().equals(child2.getName()) && this.getYear() == child2.getYear();
+        }
+        return false;
+    }
+
+    public int hashCode(){
+        return this.getYear() % 2;
+    }
+}
