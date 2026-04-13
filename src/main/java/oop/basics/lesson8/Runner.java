89459398d54141ab77d4dd8a3085f2f2aa9d156b
Almas <professor_z@mail.ru>
2026-04-13 19:43:22 +0500

    lesson8 final static

---
+++ src/main/java/oop/basics/lesson8/Runner.java
@@ -0,0 +1,12 @@
+package oop.basics.lesson8;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+        Car audi = new Car("audi");
+        Car bmw = new Car("bmw");
+        Car.BRAND_DESCRIPTION[0] = "Любое значение";
+        System.out.println(Arrays.toString(Car.BRAND_DESCRIPTION));
+    }
+}
